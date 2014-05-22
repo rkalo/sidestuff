75ff191889a70b0560e008d4f5dcb26296124e54
robertkalomiris <robertkalomiris@gmail.com>
2014-05-21 21:18:52 -0700

    fixed footballer() and addeded person()

--- footballer.js
+++ footballer.js
@@ -1,17 +1,35 @@
+function introduction(first){
+	var intro = "This is ";
+
+	function lastName(last){ 
+		return intro + first + " " + last;
+	}
+
+	return lastName;
+}
+
+//lastName is declared but not executed 
+var person = introduction("Bernie"); //[function lastName]
+
+//lastName is called, note how it has access to `intro` and `first`
+person("Mac"); //"This is Bernie Mac" 
+
+*/
+
 function footballer(first, last){
 	var goals = 0;
 
-	return {
+	return { //returns this object,
 		tally: function(){
 			return goals;
 		},
-		scoreGoal: function(){
+		score: function(){
 			goals++;
 		},
 		commentary: function(){
 			return first + " " + last + " has scored " + goals + " this season ";
 		}
-	}
+	} 
 }
 
 /*
